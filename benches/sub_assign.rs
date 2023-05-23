00af85e1fbb4dd29b69578670e1ac7278b67a378
Jonathan LEI <me@xjonathan.dev>
2023-05-23 13:18:59 +0000

    feat: subtraction benchmarks

---
+++ benches/sub_assign.rs
@@ -0,0 +1,56 @@
+use std::ops::SubAssign;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    // starknet-ff
+    {
+        use starknet_ff::FieldElement;
+
+        let mut num_1 = FieldElement::from_hex_be(
+            "0x03d937c035c878245caf64531a5756109c53068da139362728feb561405371cb",
+        )
+        .unwrap();
+        let num_2 = FieldElement::from_hex_be(
+            "0x0208a0a10250e382e1e4bbe2880906c2791bf6275695e02fbbc6aeff9cd8b31a",
+        )
+        .unwrap();
+
+        c.bench_function(
+            "sub_assign | starknet-ff - xJonathanLEI/starknet-rs@a6cbfa3",
+            |b| {
+                b.iter(|| {
+                    #[allow(clippy::unit_arg)]
+                    black_box(num_1.sub_assign(num_2));
+                });
+            },
+        );
+    }
+
+    // cairo-felt
+    {
+        use cairo_felt::Felt252;
+
+        let mut num_1 = Felt252::from_bytes_be(
+            &hex::decode("03d937c035c878245caf64531a5756109c53068da139362728feb561405371cb")
+                .unwrap(),
+        );
+        let num_2 = Felt252::from_bytes_be(
+            &hex::decode("0208a0a10250e382e1e4bbe2880906c2791bf6275695e02fbbc6aeff9cd8b31a")
+                .unwrap(),
+        );
+
+        c.bench_function(
+            "sub_assign | cairo-felt - lambdaclass/cairo-rs@e173ec9",
+            |b| {
+                b.iter(|| {
+                    #[allow(clippy::unit_arg)]
+                    black_box(num_1.sub_assign(&num_2));
+                });
+            },
+        );
+    }
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
