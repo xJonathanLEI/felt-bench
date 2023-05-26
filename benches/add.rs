d10c707649cbbdb5ec32a0891e56b6ff0bb51b9b
Jonathan LEI <me@xjonathan.dev>
2023-05-26 07:48:19 +0000

    feat: add lambdaworks

--- benches/add.rs
+++ benches/add.rs
@@ -74,6 +74,31 @@ pub fn criterion_benchmark(c: &mut Criterion) {
             });
         });
     }
+
+    // lambdaworks-math
+    {
+        use lambdaworks_math::field::{
+            element::FieldElement, fields::fft_friendly::stark_252_prime_field::Stark252PrimeField,
+        };
+
+        let num_1 = FieldElement::<Stark252PrimeField>::from_hex(
+            "03d937c035c878245caf64531a5756109c53068da139362728feb561405371cb",
+        )
+        .unwrap();
+        let num_2 = FieldElement::<Stark252PrimeField>::from_hex(
+            "0208a0a10250e382e1e4bbe2880906c2791bf6275695e02fbbc6aeff9cd8b31a",
+        )
+        .unwrap();
+
+        c.bench_function(
+            "add | lambdaworks-math - lambdaclass/lambdaworks@61b26ab",
+            |b| {
+                b.iter(|| {
+                    black_box((&num_1).add(&num_2));
+                });
+            },
+        );
+    }
 }
 
 criterion_group!(benches, criterion_benchmark);
--- benches/add_assign.rs
+++ benches/add_assign.rs
@@ -81,6 +81,34 @@ pub fn criterion_benchmark(c: &mut Criterion) {
             },
         );
     }
+
+    // lambdaworks-math
+    {
+        use lambdaworks_math::field::{
+            element::FieldElement, fields::fft_friendly::stark_252_prime_field::Stark252PrimeField,
+        };
+
+        let mut num_1 = FieldElement::<Stark252PrimeField>::from_hex(
+            "03d937c035c878245caf64531a5756109c53068da139362728feb561405371cb",
+        )
+        .unwrap();
+        let num_2 = FieldElement::<Stark252PrimeField>::from_hex(
+            "0208a0a10250e382e1e4bbe2880906c2791bf6275695e02fbbc6aeff9cd8b31a",
+        )
+        .unwrap();
+
+        c.bench_function(
+            "add_assign | lambdaworks-math - lambdaclass/lambdaworks@61b26ab",
+            |b| {
+                b.iter(|| {
+                    // We have to clone here as `FieldElement` from lambdaworks-math does not
+                    // implement `AddAssign<&FieldElement>`.
+                    #[allow(clippy::unit_arg)]
+                    black_box(num_1.add_assign(num_2.clone()));
+                });
+            },
+        );
+    }
 }
 
 criterion_group!(benches, criterion_benchmark);
